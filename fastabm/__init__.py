3da175bea78e485ac4b48ec932f611a68c0fd6f9
Rami <rami.awar.ra@gmail.com>
2021-03-22 23:09:45 +0200

    Added new rule to flake8 config

--- fastabm/__init__.py
+++ fastabm/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Rami Awar"""
 __email__ = "rami.awar.ra@gmail.com"
 __version__ = "0.0.1"
 
-from .activations import *
+from .activations import random_activation
 from .agent import Agent
 from .model import Model
