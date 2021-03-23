169b328be84c57e2b163f4ed1f0f716bf9f9b4e7
Rami <rami.awar.ra@gmail.com>
2021-03-23 10:18:04 +0200

    Added coverage table generator script

---
+++ coverage_to_md.py
@@ -0,0 +1,44 @@
+lines = None
+with open("test.txt") as f:
+    lines = f.readlines()
+
+# Find coverage
+start_index = 0
+for i, line in enumerate(lines):
+    if line.split()[0] == "Name":
+        start_index = i
+    elif line.split()[0] == "TOTAL":
+        stop_index = i
+
+# Build table header
+header = ["|"]
+separator = ["|"]
+
+headers = lines[start_index].split()
+for i, name in enumerate(headers):
+    header.append(name)
+    header.append("|")
+
+    if i == 0:
+        separator.append(":-------------")
+    elif i == len(headers):
+        separator.append("-----------:")
+    else:
+        separator.append(":----------:")
+    separator.append("|")
+
+# Build table body
+body = []
+start_index += 1
+stop_index += 1
+for line in lines[start_index:stop_index]:
+    x = ["|"]
+    for word in line.split():
+        x.extend([word, "|"])
+
+    body.append(" ".join(x))
+
+table = [" ".join(header), " ".join(separator), *body]
+
+with open("table.md", "w") as f:
+    f.write("\n".join(table))
